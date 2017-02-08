f9c17fd75da8f0cc5b518696ed0484f2adda4614
anthonyTesoriero <anthony.tesoriero>
2017-02-08 03:26:38 -0500

    Carson fix your code

--- Team9853/src/main/java/org/firstinspires/ftc/team9853/tests/GyroTest.java
+++ Team9853/src/main/java/org/firstinspires/ftc/team9853/tests/GyroTest.java
@@ -12,6 +12,15 @@ import org.firstinspires.ftc.team9853.opmodes.Auto9853;
 @Autonomous(name = "Test: Gyro", group = "Test")
 
 public class GyroTest extends Auto9853 {
+    /**
+     * setup OpMode
+     *
+     * @param isRedTeam whether or not this OpMode is for the red team.
+     */
+    public GyroTest(boolean isRedTeam) {
+        super(isRedTeam);
+    }
+
     @Override
     public void runRobot() throws StoppedException, InterruptedException {
         while(robot().driveWithHeading(Math.PI/2, .5, robot().startingHeading)) statusCheck();
